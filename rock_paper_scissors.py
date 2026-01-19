b0de28634a30f398d678ad2fb19498b43814f532
Nikolov12 <106632145+Nikolov12@users.noreply.github.com>
2026-01-19 16:51:40 +0200

    Add while cycle

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,39 +1,43 @@
 import random
+from colorama import Fore, Style, init
+
+init()
 
 rock = 'Rock'
 paper = 'Paper'
 scissors = 'Scissors'
 
+while True:  # рестартира играта
+    player_move = input(
+        Fore.CYAN + "Choose [r]ock, [p]aper or [s]cissors: " + Style.RESET_ALL
+    )
 
-player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
+    if player_move == 'r':
+        player_move = rock
+    elif player_move == 'p':
+        player_move = paper
+    elif player_move == 's':
+        player_move = scissors
+    else:
+        print(Fore.RED + "Invalid input!" + Style.RESET_ALL)
+        continue  # връща към началото на цикъла
 
-if player_move == 'r':
-    player_move = rock
-elif player_move == 'p':
-    player_move = paper
-elif player_move == 's':
-    player_move = scissors
-else:
-    raise SystemExit('Invalid Input. Try again...')
+    computer_move = random.choice([rock, paper, scissors])
 
-computer_random_number = random.randint(1, 3)
+    print(Fore.YELLOW + f"The computer chose {computer_move}." + Style.RESET_ALL)
 
-computer_move = " "
+    if (player_move == rock and computer_move == scissors) or \
+       (player_move == paper and computer_move == rock) or \
+       (player_move == scissors and computer_move == paper):
+        print(Fore.GREEN + "You win!" + Style.RESET_ALL)
+    elif player_move == computer_move:
+        print(Fore.BLUE + "Draw!" + Style.RESET_ALL)
+    else:
+        print(Fore.RED + "You lose!" + Style.RESET_ALL)
 
-if computer_random_number == 1:
-    computer_move = rock
-elif computer_random_number == 2:
-    computer_move = paper
-else:
-    computer_move = scissors
-print(f'The computer chose {computer_move}.')
+    play_again = input("\nPlay again? (y/n): ").lower()
+    if play_again != 'y':
+        print(Fore.MAGENTA + "Thanks for playing!" + Style.RESET_ALL)
+        break
 
-if (player_move == rock and computer_move == scissors) or \
-        (player_move == paper and computer_move == rock) or \
-        (player_move == scissors and computer_move == paper):
-    print("You win!")
-elif player_move == computer_move:
-    print('Draw!')
-else:
-    print('You lose!')
 
